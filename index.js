f08f64899718d9525e98810acadbde97a44833e9
Soraya Rezaie <banorezaie2004@gmail.com>
2023-11-07 10:52:24 +0430

    some changes in js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log('Hello and welcome to Javascript, Today is a beautiful day!');
 let text = "Hey my soulmate! I love you so much!";
 document.getElementById('div').innerHTML = text;
+document.getElementById('footer').innerHTML= "It is a footer"
