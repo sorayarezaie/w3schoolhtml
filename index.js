c45e1ae2b868635bcf2b3ed761dcad4436e726a2
Soraya Rezaie <banorezaie2004@gmail.com>
2023-11-11 19:20:03 +0430

    js changes

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log('Hello and welcome to Javascript, Today is a beautiful day!');
 let text = "Hey my soulmate! I love you so much!";
 document.getElementById('div').innerHTML = text;
 document.getElementById('footer').innerHTML= "It is a footer"
+document.getElementById('footer').innerHTML = "Something new has been added and updated to this site!"
