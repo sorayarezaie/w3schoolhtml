fee8243fb1f057c7d4d72cca90039fef52d5eba1
Soraya Rezaie <banorezaie2004@gmail.com>
2023-11-15 20:56:07 +0430

    this is a new javascript code

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 console.log('Hello and welcome to Javascript, Today is a beautiful day!');
-let text = "Hey my soulmate! I love you so much!";
-document.getElementById('div').innerHTML = text;
-document.getElementById('footer').innerHTML= "It is a footer"
+
+document.getElementById('btn').innerHTML = "I have been clicked!";
 document.getElementById('footer').innerHTML = "Something new has been added and updated to this site!"
